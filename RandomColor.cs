61d56926c3ab09922fa39ceee3841aa2ee1faf6d
Ash Rawstron <ash.rawstron@gmail.com>
2015-10-09 23:25:06 -0700

    Test Change on sub-module.

--- RandomColor.cs
+++ RandomColor.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class RandomColor : MonoBehaviour {
 
 	void Start () {
-		var c = new Color(Random.value, Random.value, Random.value, 1f);
+//		var c = new Color(Random.value, Random.value, Random.value, 1f);
+		var c = new Color(0, 1f, 0, 1f);
 		GetComponent<Renderer>().material.color = c;
 	}
 }
