a977091d5e8046072cdde3eda84b54987f27c977
Ash Rawstron <ash.rawstron@gmail.com>
2015-10-09 23:35:47 -0700

    test

--- RandomColor.cs
+++ RandomColor.cs
@@ -5,7 +5,7 @@ public class RandomColor : MonoBehaviour {
 
 	void Start () {
 //		var c = new Color(Random.value, Random.value, Random.value, 1f);
-		var c = new Color(0, 1f, 0, 1f);
+		var c = new Color(0, 0, 1f, 1f);
 		GetComponent<Renderer>().material.color = c;
 	}
 }
